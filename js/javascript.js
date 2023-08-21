6d467ff006f7fe73124a59fb2d200a1d2f91d1ad
unknown <gleidistonjunior@gmail.com>
2023-08-21 16:56:53 -0300

    Unificaçao dos codigos, e adiçao da pagina sobre

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,27 @@
+/*david*/
+window.addEventListener("scroll", function(){
+    let div = document.querySelector('#menu')
+    div.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+const menuDiv = document.getElementById('menu-mobile')
+const btnAnimar = document.getElementById('hamburguer')
+const btnAnimar2 = document.getElementById('icone2')
+
+
+menuDiv.addEventListener('click', animarMenu)
+btnAnimar2.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativo')
+    
+
+}
+
+
+/*JR*/
+
 var cont = 0
 var hamburguer = document.querySelector('#navbar-mobile')
 
